0d68e8f72fb630d985d58a3541764c57026516ab
BSalemi <briandsalemi@gmail.com>
2024-11-25 11:41:35 -0500

    added openHour as props to Order component for more specificity

--- src/index.js
+++ src/index.js
@@ -101,14 +101,14 @@ function Pizza({ pizzaObj }) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
+  const openHour = 11;
   const closeHour = 22;
   let isOpen = hour >= openHour && hour <= closeHour;
 
   return (
     <footer className="footer">
       {isOpen ? (
-        <Order closeHour={closeHour} />
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00.
@@ -118,10 +118,13 @@ function Footer() {
   );
 }
 
-function Order({ closeHour }) {
+function Order({ closeHour, openHour }) {
   return (
     <div className="order">
-      <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+      <p>
+        We're open from {openHour}:00 until {closeHour}:00. Come visit us or
+        order online.
+      </p>
       <button className="btn">Order</button>
     </div>
   );
